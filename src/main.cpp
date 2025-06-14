39c5a32918f1c3e279da9d8b4ad33e5822607253
Aiden Wenzel <wenzel.aiden@outlook.com>
2025-06-14 12:14:13 -0400

    move stuff

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,13 @@
 #include <cassert>
 #include <getopt.h>
 
+#include "gl_helpers.hpp"
+
 // settings
 float SCR_WIDTH = 900.0f;
 float SCR_HEIGHT = 900.0f;
 float scale = 1.0f;
 
-GLFWwindow* initialize_window(size_t width, size_t height);
-GLuint compile_shader_program(const char* vertex_shader_source, const char* fragment_shader_source);
-void read_shader_file(const std::string& filePath, std::string& shaderString);
-static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
-void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void fill_cycle_values(std::vector<float>& values, size_t n, float lower, float upper);
 
 int main(int argc, char** argv) {
@@ -27,11 +23,14 @@ int main(int argc, char** argv) {
 	glfwMakeContextCurrent(window);
 	gladLoadGL(glfwGetProcAddress);
 	
+	std::string vertex_shader_path = "../src/mandel.vert";
+	std::string frag_shader_path = "../src/julia.frag";
 	std::string vertex_shader_source;
 	std::string fragment_shader_source;
-	read_shader_file("../src/mandel.vert", vertex_shader_source);
-	read_shader_file("../src/julia.frag", fragment_shader_source);
+	read_shader_file(vertex_shader_path, vertex_shader_source);
+	read_shader_file(frag_shader_path, fragment_shader_source);
 	auto shader_program = compile_shader_program(vertex_shader_source.c_str(), fragment_shader_source.c_str());
+
 	float vertices[] = {
 		-1.0f, -1.0f, 0.0f,
 		-1.0f, 1.0f, 0.0f,
@@ -113,98 +112,6 @@ int main(int argc, char** argv) {
 	// ------------------------------------------------------------------
 	glfwTerminate();
 	return 0;
-}
-
-GLFWwindow* initialize_window(size_t width, size_t height) {
-
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Fractal Visualizer", NULL, NULL);
-	if (window == NULL) {
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		exit(-1);
-	}
-
-	return window;
-}
-
-GLuint compile_shader_program(const char* vertex_shader_source, const char* fragment_shader_source) {
-	// vertex shader
-	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertex_shader_source, NULL);
-	glCompileShader(vertexShader);
-	// check for shader compile errors
-	int success;
-	char infoLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
-
-	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragment_shader_source, NULL);
-	glCompileShader(fragmentShader);
-	// check for shader compile errors
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
-	// link shaders
-	unsigned int shader_program = glCreateProgram();
-	glAttachShader(shader_program, vertexShader);
-	glAttachShader(shader_program, fragmentShader);
-	glLinkProgram(shader_program);
-	// check for linking errors
-	glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
-	if (!success) {
-		glGetProgramInfoLog(shader_program, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-	}
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-	return shader_program;
-}
-
-void read_shader_file(const std::string& filePath, std::string& shaderString) {
-	std::fstream fileStream(filePath);
-	if (!fileStream.is_open()) {
-		std::cout << "Could not open file " << filePath << "\n";
-		exit(-2);
-	}
-
-	std::string tmp;
-	while (std::getline(fileStream, tmp)) {
-		shaderString.append(tmp + "\n");
-	}
-	
-	fileStream.close();
-}
-
-static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos) {
-
-}
-
-void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
- //    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS) {
-	// 	std::cout << "Pressing\n";
-	// }
-}
-
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
-    if (yoffset > 0) {
-        // Scrolled up (zooming in)
-		scale *= 0.8;
-    } else if (yoffset < 0) {
-        // Scrolled down (zooming out)
-		scale *= 1.2;
-    }
 }
 
 void fill_cycle_values(std::vector<float>& values, size_t n, float lower, float upper) {
