b95fe80a0495cc58eeb39694077c70392657d987
Aiden Wenzel <wenzel.aiden@outlook.com>
2025-10-14 00:06:42 -0400

    write compile_shader()

--- src/shader.cpp
+++ src/shader.cpp
@@ -31,3 +31,45 @@ Shader::Shader(const std::string& frag_shader_path, const std::string& vert_shad
 	
 	vert_stream.close();
 }
+
+void Shader::compile_shader() {
+	// vertex shader
+	const char* vert_src = this->vert_src.c_str();
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vert_src, NULL);
+	glCompileShader(vertexShader);
+	// check for shader compile errors
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	const char* frag_src = this->frag_src.c_str();
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &frag_src, NULL);
+	glCompileShader(fragmentShader);
+	// check for shader compile errors
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	// link shaders
+	unsigned int shader_program = glCreateProgram();
+	glAttachShader(shader_program, vertexShader);
+	glAttachShader(shader_program, fragmentShader);
+	glLinkProgram(shader_program);
+	// check for linking errors
+	glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shader_program, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	this->compiled_shader = shader_program;
+}
--- src/shader.hpp
+++ src/shader.hpp
@@ -1,11 +1,17 @@
 #include <string>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #pragma once
 
 class Shader {
 public:
 	Shader(const std::string& frag_shader_path, const std::string& vert_shader_path);
 private:
+	void compile_shader();
+
 	std::string frag_src;
 	std::string vert_src;
+	GLuint compiled_shader;
 };
