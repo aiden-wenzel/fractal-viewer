12db63b2ba4d652cd5906e45595ba6f956724841
Aiden Wenzel <wenzel.aiden@outlook.com>
2025-10-14 13:00:07 -0400

    remove unessesary functions

--- include/frac/gl_helpers.hpp
+++ include/frac/gl_helpers.hpp
@@ -1,8 +1,4 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include <string>
-
 GLFWwindow* initialize_window(size_t width, size_t height);
-GLuint compile_shader_program(const char* vertex_shader_source, const char* fragment_shader_source);
-void read_shader_file(const std::string& filePath, std::string& shaderString);
