8b55c85fd2c74b7c1752005953482d318e918d19
Aiden Wenzel <wenzel.aiden@outlook.com>
2025-10-24 20:28:26 -0400

    check if mouse is being pressed to move

--- src/mouse.cpp
+++ src/mouse.cpp
@@ -19,7 +19,15 @@ void Mouse::poll_current_mouse_pos(GLFWwindow* window) {
 	this->prev_mouse_pos = this->curr_mouse_pos;
 	glfwGetCursorPos(window, &this->curr_mouse_pos[0], &this->curr_mouse_pos[1]);
 
-	this->mouse_diff[0] = this->curr_mouse_pos[0] - this->prev_mouse_pos[0];
-	this->mouse_diff[1] = this->curr_mouse_pos[1] - this->prev_mouse_pos[1];
+
+	int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+	if (state == GLFW_PRESS) {
+		this->mouse_diff[0] = this->curr_mouse_pos[0] - this->prev_mouse_pos[0];
+		this->mouse_diff[1] = this->curr_mouse_pos[1] - this->prev_mouse_pos[1];
+	}
+	else {
+		this->mouse_diff[0] = 0;
+		this->mouse_diff[1] = 0;
+	}
 	std::cout << this->mouse_diff[0] << " " << this->mouse_diff[1] << "\n";
 }
